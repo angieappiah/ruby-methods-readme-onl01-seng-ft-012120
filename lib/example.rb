3e092e0250a9e2ddf4475e55d106342821b32a87
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-18 14:52:25 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,16 +1,7 @@
-# Use this space to code along with the readme
-def say_hello_world_ten_times
+
+def greeting
 phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+puts say {"phrase"} ten_times
 end
 
 
